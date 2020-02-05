5a41b1636ac7c4d5c3e29401346aeab6c6878f08
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2020-02-05 16:05:08 +0700

    tes render html tag

--- application/views/welcome_message.blade.php
+++ application/views/welcome_message.blade.php
@@ -13,9 +13,9 @@
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="javascript:void(0)">User Guide</a>.</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+<p class="footer">Page rendered in <strong></strong> seconds. {{!!  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' !!}}</p>
 </div>
 @stop
